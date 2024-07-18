3bac13bca7ffc261c50ab1f4ce37841fc19e7b50
VictorFerreira999 <victorferreira9990@gmail.com>
2024-07-17 21:11:39 -0300

    definindo tipos

--- typescript/dist/index.js
+++ typescript/dist/index.js
@@ -1,5 +1,44 @@
 "use strict";
-const numero1 = 4;
-const numero2 = 3;
-console.log('TYPESCRIPT');
-console.log(numero1 + numero2);
+let numero = 20;
+const pi = 3.1415;
+let nome = 'WALISSON';
+let correta;
+let resultado = numero * pi;
+const numeros = [1, 2, 3, 4];
+//const misto: (number | string)[] = ['Victor', 27, 1.77] //Evitar misturar tipos
+const idades = [];
+idades.push(23);
+idades.push(42);
+idades.push(15);
+idades.push(17);
+idades.push(55);
+const menorDeIdade = idades.filter((idade) => idade < 18);
+console.log(menorDeIdade);
+const pessoaTupla = ['Victor', 25]; //"Tupla"
+//Object Types
+//Interface
+// interface Person {
+//    nome: string,
+//    idade: number,
+//    profissao?: string,
+//    altura: number
+// }
+const pessoa = {
+    nome: 'Victor',
+    idade: 25,
+    profissao: 'Desenvolvedor',
+    altura: 1.75
+};
+function chooseNumber(numero1, numero2, criterio) {
+    switch (criterio) {
+        case 'greater':
+            return numero1 > numero2 ? numero1 : numero2;
+        case 'lower':
+            return numero1 < numero2 ? numero1 : numero2;
+        default:
+            const numeroAleatorio = Math.random();
+            return numeroAleatorio >= 0.5 ? numero1 : numero2;
+    }
+}
+const numeroEscolhido = chooseNumber(10, 20);
+console.log("numeroEscolhido", numeroEscolhido);
--- typescript/index.ts
+++ typescript/index.ts
@@ -1,5 +1,66 @@
-const numero1 = 4
-const numero2 = 3
+let numero = 20
 
-console.log('TYPESCRIPT')
-console.log(numero1 + numero2)
+const pi =  3.1415
+
+let nome: string = 'WALISSON'
+
+let correta: boolean
+
+let resultado = numero * pi
+
+const numeros: number[] = [1,2,3,4]
+
+//const misto: (number | string)[] = ['Victor', 27, 1.77] //Evitar misturar tipos
+
+const idades: number[] = []
+idades.push(23)
+idades.push(42)
+idades.push(15)
+idades.push(17)
+idades.push(55)
+
+const menorDeIdade = idades.filter((idade) => idade < 18)
+console.log(menorDeIdade)
+
+const pessoaTupla: [string, number] = ['Victor', 25] //"Tupla"
+
+//Object Types
+
+//Interface
+
+// interface Person {
+//    nome: string,
+//    idade: number,
+//    profissao?: string,
+//    altura: number
+// }
+
+const pessoa: Person = {
+   nome: 'Victor',
+   idade: 25,
+   profissao: 'Desenvolvedor',
+   altura: 1.75
+}
+
+type Person = {
+   nome: string,
+   idade: number,
+   profissao?: string,
+   altura: number
+}
+
+function chooseNumber(numero1: number, numero2: number, criterio?: 'greater' | 'lower'): number {
+   switch (criterio) {
+       case 'greater':
+           return numero1 > numero2 ? numero1 : numero2;
+       case 'lower':
+           return numero1 < numero2 ? numero1 : numero2;
+       default:
+           const numeroAleatorio = Math.random();
+           return numeroAleatorio >= 0.5 ? numero1 : numero2;
+   }
+}
+
+const numeroEscolhido = chooseNumber(10, 20);
+
+console.log("numeroEscolhido", numeroEscolhido);
