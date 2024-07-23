464c92c4f90668f78e2a386235bf181c2b131140
VictorFerreira999 <victorferreira9990@gmail.com>
2024-07-23 09:49:02 -0300

    Rotasw

--- react/my-react-app/src/App.jsx
+++ react/my-react-app/src/App.jsx
@@ -1,12 +1,22 @@
-import Titulo from "./Titulo"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Home from "../pages/Home"
+import Sobre from "../pages/Sobre"
+import Contato from "../pages/Contato"
+
+
 
 function App() {
   return (
     <div>
-      <Titulo cor = "red"/>
-      <Titulo cor = "blue"/>
-      <Titulo cor = "purple"/>
-      <Titulo cor = "orangered"/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element= {<Home />}/>
+          <Route path="/Sobre" element= {<Sobre />}/>
+          <Route path="/Contato" element= {<Contato />}/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 
